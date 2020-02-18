0098f0b1b5cdc63b271294d72c8e4924d359ef12
Ismail Dogan <ismaildogaan@gmail.com>
2020-02-19 00:17:34 +0300

    SeizeTheDay.Api layer has been created.

---
+++ SeizeTheDay.Api/App_Start/BundleConfig.cs
@@ -0,0 +1,16 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace SeizeTheDay.Api
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+        }
+    }
+}
---
+++ SeizeTheDay.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SeizeTheDay.Api.WebApiApplication" Language="C#" %>
