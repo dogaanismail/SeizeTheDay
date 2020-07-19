2e0cc0ec8dcd7d32fbb91b9db77fc7493e0506c0
ismail doğan <ismaildogaan@gmail.com>
2020-07-19 18:17:21 +0300

    Admin layer has been added into the system.

---
+++ SeizeTheDay.Admin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SeizeTheDay.Admin.MvcApplication" Language="C#" %>
---
+++ SeizeTheDay.Admin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SeizeTheDay.Admin.Startup))]
+namespace SeizeTheDay.Admin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
